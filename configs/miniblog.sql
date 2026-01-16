c2e4a35fadc54de156584dba9b4eee851216811e
mervyn <mervyn@163.com>
2026-01-16 10:33:57 +0800

    Initial commit: MiniBlog codebase
    - Port code from github.com/onexstack/miniblog
    - Update module path to github.com/ashwinyue/eino-show
    - Update all package imports (129 files modified)

---
+++ configs/miniblog.sql
@@ -0,0 +1,145 @@
+-- Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
+-- Use of this source code is governed by a MIT style
+-- license that can be found in the LICENSE file. The original repo for
+-- this file is https://github.com/ashwinyue/eino-show. The professional
+-- version of this repository is https://github.com/onexstack/onex.
+
+-- MariaDB dump 10.19-11.2.2-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 10.37.91.93    Database: miniblog
+-- ------------------------------------------------------
+-- Server version	10.11.6-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `miniblog`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `miniblog`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `miniblog` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+
+USE `miniblog`;
+
+--
+-- Table structure for table `casbin_rule`
+--
+
+DROP TABLE IF EXISTS `casbin_rule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `casbin_rule` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `ptype` varchar(100) DEFAULT NULL,
+  `v0` varchar(100) DEFAULT NULL,
+  `v1` varchar(100) DEFAULT NULL,
+  `v2` varchar(100) DEFAULT NULL,
+  `v3` varchar(100) DEFAULT NULL,
+  `v4` varchar(100) DEFAULT NULL,
+  `v5` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_casbin_rule` (`ptype`,`v0`,`v1`,`v2`,`v3`,`v4`,`v5`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `casbin_rule`
+--
+
+LOCK TABLES `casbin_rule` WRITE;
+/*!40000 ALTER TABLE `casbin_rule` DISABLE KEYS */;
+INSERT INTO `casbin_rule` VALUES
+(18,'g','user-000000','role::admin',NULL,NULL,'',''),
+(21,'p','role::admin','*','*','allow','',''),
+(7,'p','role::user','/v1.MiniBlog/DeleteUser','CALL','deny','',''),
+(8,'p','role::user','/v1.MiniBlog/ListUser','CALL','deny','',''),
+(9,'p','role::user','/v1/users','GET','deny','',''),
+(10,'p','role::user','/v1/users/*','DELETE','deny','','');
+/*!40000 ALTER TABLE `casbin_rule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` varchar(36) NOT NULL DEFAULT '' COMMENT '用户唯一 ID',
+  `postID` varchar(35) NOT NULL DEFAULT '' COMMENT '博文唯一 ID',
+  `title` varchar(256) NOT NULL DEFAULT '' COMMENT '博文标题',
+  `content` longtext NOT NULL DEFAULT '' COMMENT '博文内容',
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp() COMMENT '博文创建时间',
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '博文最后修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `post.postID` (`postID`),
+  KEY `idx.post.userID` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci COMMENT='博文表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` varchar(36) NOT NULL DEFAULT '' COMMENT '用户唯一 ID',
+  `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名（唯一）',
+  `password` varchar(255) NOT NULL DEFAULT '' COMMENT '用户密码（加密后）',
+  `nickname` varchar(30) NOT NULL DEFAULT '' COMMENT '用户昵称',
+  `email` varchar(256) NOT NULL DEFAULT '' COMMENT '用户电子邮箱地址',
+  `phone` varchar(16) NOT NULL DEFAULT '' COMMENT '用户手机号',
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp() COMMENT '用户创建时间',
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '用户最后修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user.userID` (`userID`),
+  UNIQUE KEY `user.username` (`username`),
+  UNIQUE KEY `user.phone` (`phone`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES
+(96,'user-000000','root','$2a$10$ctsFXEUAMd7rXXpmccNlO.ZRiYGYz0eOfj8EicPGWqiz64YBBgR1y','colin404','colin404@foxmail.com','18110000000','2024-12-12 03:55:25','2024-12-12 03:55:25');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-12  4:33:51
